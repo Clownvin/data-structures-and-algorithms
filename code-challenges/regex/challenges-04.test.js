6b63b878461706bd1f17310852e56663aee509ee
Calvin G Hall <123chall@gmail.com>
2019-08-15 16:28:47 -0500

    small change to stretch

--- code-challenges/regex/challenges-04.test.js
+++ code-challenges/regex/challenges-04.test.js
@@ -95,7 +95,7 @@ Hint: All of these words end with the letters "ells".
 
 const seashells = 'She sells seashells by the seashore. The shells she sells are surely seashells. So if she sells shells on the seashore, I\'m sure she sells seashore shells.';
 
-const findShells = (str) => str.match(/\b\w*ells/g);
+const findShells = (str) => str.match(/\bs(eas)?h?\w*ells/g);
 
 /* ------------------------------------------------------------------------------------------------
 TESTS

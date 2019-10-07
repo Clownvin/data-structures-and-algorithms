c63311e33d7a18adb0f302aa3ae34b0d255786a6
Calvin G Hall <123chall@gmail.com>
2019-10-07 16:20:17 -0500

    FizzBuzzTree

---
+++ code-challenges/fizzBuzzTree/README.md
@@ -0,0 +1,9 @@
+# FizzBuzzTree
+It's a function that takes a tree, and "fizz-buzzes" each node in the tree (multiple of 3? fizz. Multiple of 5? buzz. Multiple of 15? fizzbuzz. Else? same value)
+
+## Approach & Efficiency
+Wrote a function that uses the preOrder method to fizzbuzz each node, then returns the tree.
+
+## API
+### fizzBuzzTree(tree)
+Returns the tree after fizz-buzzing each node.
--- code-challenges/fizzBuzzTree/test/fizz-buzz-tree.test.js
+++ code-challenges/fizzBuzzTree/test/fizz-buzz-tree.test.js
@@ -1,3 +1,5 @@
+'use-strict';
+const randomEmoji = require('../../../randomojis/randomojis');
 const fizzBuzzTree = require('../fizz-buzz-tree');
 const {BinarySearchTree} = require('../../../tree/tree');
 
@@ -7,31 +9,38 @@ function fizzBuzzArray(arr) {
 
 function getRandomNumber(trueRandom) {
   if (trueRandom) {
-    return Number.MAX_VALUE - (Math.random() * (Number.MAX_VALUE / 2));
+    return Math.floor(Number.MAX_VALUE - (Math.random() * (Number.MAX_VALUE / 2)));
   }
   const rand = Math.random();
   let base = rand < .333 ? 3 : rand < .666 ? 5 : 15;
-  base *= Math.random() * 10;
+  base *= Math.floor(Math.random() * 10);
   return base;
 }
 
 describe('FizzBuzzTree', () => {
+  let tree;
+
+  beforeEach(() => {
+    tree = new BinarySearchTree();
+  });
 
   it('Throws an error if no tree provided', () => {
     expect(() => fizzBuzzTree(null)).toThrow();
   });
 
-  for (let i = 0; i < 100; i++) {
-    let tree;
-    for (let j = 0; j < 10; j++) {
-      tree = new BinarySearchTree();
-      for (let k = 0; k < i; k++) {
-        tree.add(getRandomNumber(j > 4 ? true : false));
+  for (let i = 1; i <= 100; i++) {
+    const emoji = randomEmoji();
+    it(`${emoji} Works for trees with ${i} nodes ${emoji}`, () => {
+      for (let k = 0; k < 10; k++) {
+        tree = new BinarySearchTree();
+        for (let j = 0; j < i; j++) {
+          tree.add(getRandomNumber(k > 5));
+        }
+        const expected = fizzBuzzArray(tree.inOrder());
+        const got = fizzBuzzTree(tree).inOrder();
+        //console.log(`Expecting ${expected} to equal ${got}`);
+        expect(got).toStrictEqual(expected);
       }
-      describe(`BST with ${i} random values ${j > 4 ? ', all multiples of 3, 5, or 15' : ', all totally random'}`, () => {
-        const expectedInOrder = fizzBuzzArray(tree.inOrder());
-        expect(fizzBuzzTree(tree).inOrder()).toStrictEqual(expectedInOrder);
-      });
-    }
+    });
   }
 });
